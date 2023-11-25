f34f16f8caf955890415c0fe644005f2efb96fe4
jpwilson <jpwilson@hey.com>
2023-11-25 10:39:34 -0600

    Add search bar to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,30 @@
-import { HStack, Heading, Image } from "@chakra-ui/react";
+import { Flex, Box, Heading, Image, Input } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { Link } from "react-router-dom";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="14px">
-      <HStack>
+    <Flex justifyContent="space-between" padding="14px" align="center">
+      <Flex align="center" minW="200px">
+        {" "}
+        {/* Adjust the minW as needed */}
         <Link to="/">
           <Image src={logo} boxSize="60px" borderRadius={8} />
         </Link>
-        <Heading color={"lightgreen"}>EEVEE</Heading>
-      </HStack>
-      <Heading>Electric Vehical Database</Heading>
+        <Heading color={"lightgreen"} ml={3}>
+          EEVEE
+        </Heading>
+      </Flex>
+      <Box flex="1" mx={3}>
+        {" "}
+        {/* Search bar will grow to fill available space */}
+        {/* <Input borderRadius={20} placeholder="Search EVs..." variant="filled" /> */}
+        <SearchInput />
+      </Box>
       <ColorModeSwitch />
-    </HStack>
+    </Flex>
   );
 };
 
