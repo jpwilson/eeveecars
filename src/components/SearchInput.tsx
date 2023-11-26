b33841bc2b3cea502f056b7b9150858788c5916e
jpwilson <jpwilson@hey.com>
2023-11-26 09:25:03 -0600

    Implement search bar clear button

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,38 +1,86 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import { useRef } from "react";
-import { BsSearch } from "react-icons/bs";
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  IconButton,
+} from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import { BsSearch, BsX } from "react-icons/bs";
 
 interface Props {
   onSearch: (searchText: string) => void;
 }
 
 const SearchInput = ({ onSearch }: Props) => {
+  const [value, setValue] = useState("");
   const ref = useRef<HTMLInputElement>(null);
 
   const handleInputChange = (event) => {
-    const currentText = event.target.value;
-    onSearch(currentText); // Assuming onSearch updates the search state in your parent component
+    setValue(event.target.value);
+    onSearch(event.target.value); // This triggers the search in the parent component
+  };
+
+  const handleClearSearch = () => {
+    setValue("");
+    onSearch("");
+    ref.current?.focus();
   };
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (ref.current) onSearch(ref.current.value);
-      }}
-    >
-      <InputGroup>
-        <InputLeftElement children={<BsSearch />} />
-        <Input
-          ref={ref}
-          borderRadius={20}
-          placeholder="Search EV makes, models and submodels"
-          variant="filled"
-          onChange={handleInputChange} // Add this line
-        />
-      </InputGroup>
-    </form>
+    <InputGroup>
+      <InputLeftElement pointerEvents="none" children={<BsSearch />} />
+      <Input
+        ref={ref}
+        value={value}
+        borderRadius={20}
+        placeholder="Search EV makes, models and submodels"
+        variant="filled"
+        onChange={handleInputChange}
+      />
+      {value && (
+        <InputRightElement>
+          <IconButton
+            aria-label="Clear search"
+            icon={<BsX />}
+            size="sm"
+            onClick={handleClearSearch}
+          />
+        </InputRightElement>
+      )}
+    </InputGroup>
   );
 };
 
 export default SearchInput;
+
+// const SearchInput = ({ onSearch }: Props) => {
+//   const ref = useRef<HTMLInputElement>(null);
+
+//   const handleInputChange = (event) => {
+//     const currentText = event.target.value;
+//     onSearch(currentText); // Assuming onSearch updates the search state in your parent component
+//   };
+
+//   return (
+//     <form
+//       onSubmit={(event) => {
+//         event.preventDefault();
+//         if (ref.current) onSearch(ref.current.value);
+//       }}
+//     >
+//       <InputGroup>
+//         <InputLeftElement children={<BsSearch />} />
+//         <Input
+//           ref={ref}
+//           borderRadius={20}
+//           placeholder="Search EV makes, models and submodels"
+//           variant="filled"
+//           onChange={handleInputChange} // Add this line
+//         />
+//       </InputGroup>
+//     </form>
+//   );
+// };
+
+// export default SearchInput;
