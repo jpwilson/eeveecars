52fe4cf1edf35deb58c8a8e9b6e44ef9d2dd4e30
jpwilson <jpwilson@hey.com>
2023-12-06 13:09:43 -0600

    debugging GA

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -7,8 +7,10 @@ const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID || "";
 // Initialize ReactGA
 if (TRACKING_ID) {
   ReactGA.initialize(TRACKING_ID);
+  console.log("The WOKRING tridis: " + TRACKING_ID.slice(0, 4));
 } else {
   console.error("Google Analytics tracking ID not set");
+  console.log("The NOT WORKING tridis: " + TRACKING_ID.slice(0, 4));
 }
 
 const GoogleAnalytics = () => {
