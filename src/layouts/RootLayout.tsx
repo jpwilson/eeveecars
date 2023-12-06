64833fc7a955d8187c5e8ec4e3a8940eeaaf5e06
jpwilson <jpwilson@hey.com>
2023-12-06 13:04:35 -0600

    vercel analytics

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import GoogleAnalytics from "../components/GoogleAnalytics";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout() {
   return (
     <div>
+      <Analytics />
       <GoogleAnalytics />
       <Outlet />
     </div>
