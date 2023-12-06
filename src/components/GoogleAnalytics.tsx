e348f5c293830a419c2b9d6a2456d5f5287b3b62
jpwilson <jpwilson@hey.com>
2023-12-06 13:22:07 -0600

    update old ga pkg to ga-4

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID || "";
 
 // Initialize ReactGA
 if (TRACKING_ID) {
   ReactGA.initialize(TRACKING_ID);
-  console.log("The WOKRING tridis: " + TRACKING_ID.slice(0, 4));
+  //   console.log("The WOKRING tridis: " + TRACKING_ID.slice(0, 4));
 } else {
   console.error("Google Analytics tracking ID not set");
-  console.log("The NOT WORKING tridis: " + TRACKING_ID.slice(0, 4));
+  //   console.log("The NOT WORKING tridis: " + TRACKING_ID.slice(0, 4));
 }
 
 const GoogleAnalytics = () => {
@@ -18,7 +18,11 @@ const GoogleAnalytics = () => {
 
   useEffect(() => {
     if (TRACKING_ID) {
-      ReactGA.pageview(location.pathname + location.search);
+      // Update for GA4 page view tracking
+      ReactGA.send({
+        hitType: "pageview",
+        page: location.pathname + location.search,
+      });
     }
   }, [location]);
 
